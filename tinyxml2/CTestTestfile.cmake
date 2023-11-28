d1a3ca34de0f831743e491c8e61f6a4ee867c9eb
Felipe Manga <fmanga@macmini.Home>
2023-11-28 13:29:16 +0000

    Add tinyxml2

---
+++ tinyxml2/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/fmanga/Projects/LibreSprite/android/tinyxml2
+# Build directory: /Users/fmanga/Projects/LibreSprite/android/tinyxml2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "/Users/fmanga/Projects/LibreSprite/android/app/build/intermediates/cxx/RelWithDebInfo/5y154h54/obj/x86_64/xmltest")
+set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/Users/fmanga/Projects/LibreSprite/android/tinyxml2" _BACKTRACE_TRIPLES "/Users/fmanga/Projects/LibreSprite/android/tinyxml2/CMakeLists.txt;48;add_test;/Users/fmanga/Projects/LibreSprite/android/tinyxml2/CMakeLists.txt;0;")
