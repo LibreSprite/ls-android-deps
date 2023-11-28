5b853dd11727d9208797989267f8ef0cc1539c46
Felipe Manga <fmanga@macmini.Home>
2023-11-28 16:04:51 +0000

    Fullscreen buttonless hack

--- app/src/main/java/rocks/georgik/sdlapp/MainActivity.java
+++ app/src/main/java/rocks/georgik/sdlapp/MainActivity.java
@@ -166,6 +166,19 @@ public class MainActivity extends Activity {
         // Set up the surface
         mSurface = new SDLSurface(getApplication());
 
+        Window window = getWindow();
+        if (window != null) {
+            int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
+            window.getDecorView().setSystemUiVisibility(flags);
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        }
+
         if(Build.VERSION.SDK_INT >= 12) {
             mJoystickHandler = new SDLJoystickHandler_API12();
         }
